0dfce68a627446dd71a52fc05d0d1878e2922ed3
jeon <jejjb06@gmail.com>
2024-09-09 01:58:40 +0900

    문의사항하는중

---
+++ src/main/java/flower/dreamie/qna/controller/QnaController.java
@@ -0,0 +1,22 @@
+package flower.dreamie.qna.controller;
+
+import flower.dreamie.qna.service.QnaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class QnaController {
+
+    private final QnaService qnaService;
+
+    public QnaController(QnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+
+    @RequestMapping("/qna")
+    public String list(Model model) {
+        model.addAttribute("qnaList", qnaService.getAllQnaList());
+        return "qna/qnaList";
+    }
+}
